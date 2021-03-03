0bf956313b06264f29b324637d084e71168b5fa1
Caroline Fassot <fassot.caroline@gmail.com>
2021-03-03 15:54:08 +0100

    nettoyage du plugin bootstrap

--- src/main.js
+++ src/main.js
@@ -4,26 +4,8 @@ import App from './App.vue'
 import "./plugins/vue2-leaflet";
 import "./plugins/firebase";
 import "./plugins/vuefire";
-
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "./plugins/bootstrapVue";
 import router from './router'
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-
-
-// import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
-// import 'leaflet/dist/leaflet.css';
-//
-// Vue.component('l-map', LMap);
-// Vue.component('l-tile-layer', LTileLayer);
-// Vue.component('l-marker', LMarker);
 
 
 Vue.config.productionTip = false
