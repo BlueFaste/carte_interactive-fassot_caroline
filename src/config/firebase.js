96ab16cfcf602e26e13d9d95706eff39c13c2185
Caroline Fassot <fassot.caroline@gmail.com>
2021-03-03 15:20:47 +0100

    mise en place des plugins

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCcKZwJQiyKaCOWF63iMoQlZ5myGlpVHto",
+	authDomain: "carte-interactive-vue.firebaseapp.com",
+	databaseURL: "https://carte-interactive-vue-default-rtdb.firebaseio.com",
+	projectId: "carte-interactive-vue",
+	storageBucket: "carte-interactive-vue.appspot.com",
+	messagingSenderId: "595237948446",
+	appId: "1:595237948446:web:9369e7e6de22ab6c20f20f"
+};
+export default firebaseConfig;
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "./plugins/vue2-leaflet";
+import "./plugins/firebase";
+import "./plugins/vuefire";
+
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+// import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+// import 'leaflet/dist/leaflet.css';
+//
+// Vue.component('l-map', LMap);
+// Vue.component('l-tile-layer', LTileLayer);
+// Vue.component('l-marker', LMarker);
+
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App),
 }).$mount('#app')
